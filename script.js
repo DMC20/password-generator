06cc3861f50f884ad854cff75703e8b4150d4fd9
Daniel Carazo <danielcarazo20@gmail.com>
2021-06-09 10:05:40 -0400

    added conditional statements

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 // Assignment code here
-var alpha
-var specialCharacters
-var numbers
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -19,13 +16,38 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
+
+  
+  var tempPassword = '';
+
+  // prompts in regards to passwords  decisions 
   var passwordLength = prompt("Please enter the number of characters you want for you new password. It must be at least 8 characters, no longer than 128 characters");
 
   var numbers = confirm("Do you want numbers in your password?");
 
-  var lowerCases = confirm("Do you want lowercases in your password?");
+  var lowerChar = confirm("Do you want lowercases in your password?");
 
-  var upperCases = confirm("Do you want uppercases in your password?");
+  var upperChar = confirm("Do you want uppercases in your password?");
 
-  var special = confirm("Do you want special characters in your password?");
+  var specialChar = confirm("Do you want special characters in your password?");
+
+  var minCount = 8;
+
+  if (numbers === true) {
+
+  }
+
+  if (lowerChar === true) {
+
+  }
+
+  if (upperChar === true) {
+
+  }
+
+  if (specialChar === true) {
+
+  }
+
+  return tempPassword;
 };
