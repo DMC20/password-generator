b225f893952526dd6221e19c94fef50be83b5ce6
Daniel Carazo <danielcarazo20@gmail.com>
2021-06-10 18:47:14 -0400

    added for loop

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment code here
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,14 +15,18 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var specialNum = ['!', '@', '#']
+
 function generatePassword(){
 
-  
-  var tempPassword = '';
+  var tempPassword = [];
 
   // prompts in regards to passwords  decisions 
-  var passwordLength = prompt("Please enter the number of characters you want for you new password. It must be at least 8 characters, no longer than 128 characters");
-
+  var passLength = Number(prompt("How many characters would you like your password to be?"));
+  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be between 8-128 characters. How many characters would you like your password to be?"));
+  
   var numbers = confirm("Do you want numbers in your password?");
 
   var lowerChar = confirm("Do you want lowercases in your password?");
@@ -31,23 +35,67 @@ function generatePassword(){
 
   var specialChar = confirm("Do you want special characters in your password?");
 
-  var minCount = 8;
+  // min char length for password
+  var minLength = 8;
 
-  if (numbers === true) {
 
+
+  // // conditonal statements
+  // if (numbers === true) {
+
+  // }
+
+  // if (lowerChar === true) {
+
+  // }
+
+  // if (upperChar === true) {
+
+  // }
+
+  // if (specialChar === true) {
+
+  // }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
   }
 
-  if (lowerChar === true) {
+  // console.error(getRandomInt(10))
+
+  // var tempNumPass = getRandomInt(minLength);
+
+  for (var i = 0; i < minLength; i++) {
+    console.error(minLength)
+    var numberHolder = getRandomInt(number.length);
+    tempPassword.push(number[numberHolder])
+    var charHolder = getRandomInt(specialNum.length);
+    tempPassword.push(specialNum[charHolder]);
+    console.log(tempPassword)
 
   }
-
-  if (upperChar === true) {
-
-  }
-
-  if (specialChar === true) {
-
-  }
+  
 
   return tempPassword;
 };
+
+ 
+  function getNum() {
+    return String.fromCharCode(Math.floor(Math.random() * 8 + 40));
+  };
+
+  function getLowerChar() {
+    return String.fromCharCode(Math.floor(Math.random() * 20 + 60));
+  };
+
+  function getUpperChar() {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 60));
+  };
+
+  //function specialChar() {
+   // return String.fromCharCode(Math.floor(Math.random() * ))
+  //};
+
+  function checkLength (pass) {
+    
+  } 
